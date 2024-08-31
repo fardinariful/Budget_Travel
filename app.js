2830bf59ce4463d02f1d09c49ebc108877bacd21
fardinariful <fardinewucse2020@gmail.com>
2024-08-31 21:30:34 +0600

    thrid commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const allbtn=document.getElementsByClassName("all-btn");
 let count=0;
 
 for(const btn of allbtn){
-    btn.addEventListener("click",function(){
+    btn.addEventListener("click",function(e){
         count=count+1;
         document.getElementById('cart_count').innerText=count;
         
