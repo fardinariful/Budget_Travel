9f33be66ce2a3a759338ca75446065e142c37f55
fardinariful <fardinewucse2020@gmail.com>
2024-09-01 18:30:13 +0600

    something added

--- app.js
+++ app.js
@@ -7,7 +7,25 @@ for(const btn of allbtn){
     btn.addEventListener("click",function(e){
         count=count+1;
 
-        console.log(e.target.parentNode.childNodes[1].innerText);
+        const placeName=e.target.parentNode.childNodes[1].innerText;
+        const cost=e.target.parentNode.childNodes[3].childNodes[1].innerText
+
+       const selectedContainer=document.getElementById("selected-place-container");
+    //    selectedContainer.style.display = "flex";
+    //    selectedContainer.style.flexDirection="row";
+
+       const li=document.createElement("li");
+       li.style.display="flex";
+       li.style.flexDirection="row";
+       const p=document.createElement("p");
+       const p2=document.createElement("p2");
+       p.innerText=placeName;
+       p2.innerText=cost;
+
+       li.appendChild(p);
+       li.appendChild(p2);
+       selectedContainer.appendChild(li);
+       
 
         setInnerText("cart_count",count);
         
