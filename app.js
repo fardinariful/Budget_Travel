14876df3b4509f828b832f18add7cc777c4d263b
fardinariful <fardinewucse2020@gmail.com>
2024-09-02 14:56:07 +0600

    something added

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ for(const btn of allbtn){
         const cost=e.target.parentNode.childNodes[3].childNodes[1].innerText
 
        const selectedContainer=document.getElementById("selected-place-container");
-    //    selectedContainer.style.display = "flex";
-    //    selectedContainer.style.flexDirection="row";
+ 
 
        const li=document.createElement("li");
        li.style.display="flex";
        li.style.flexDirection="row";
+       li.style.gap="10px";
        const p=document.createElement("p");
        const p2=document.createElement("p2");
        p.innerText=placeName;
@@ -26,11 +26,18 @@ for(const btn of allbtn){
        li.appendChild(p2);
        selectedContainer.appendChild(li);
        
+       const total_cost=document.getElementById("Total_costs").innerText;
+       const convertedtotalcost=parseInt(total_cost);
+       document.getElementById("Total_costs").innerText=convertedtotalcost+parseInt(cost);
+
+
 
         setInnerText("cart_count",count);
         
 })
 }
+
+
 
 function setInnerText(id,value){
     document.getElementById(id).innerText=value;
