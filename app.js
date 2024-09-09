a19ac401dbbab4704bcc507c7a219cd2ec5ffc70
fardinariful <fardinewucse2020@gmail.com>
2024-09-09 17:58:52 +0600

    something added

--- app.js
+++ app.js
@@ -1,48 +1,100 @@
-
-
 const allbtn=document.getElementsByClassName("all-btn");
 let count=0;
 
-for(const btn of allbtn){
-    btn.addEventListener("click",function(e){
-        count=count+1;
+ for(const btn of allbtn){
+    btn.addEventListener("click",function(){
+      count=count+1;
 
-        const placeName=e.target.parentNode.childNodes[1].innerText;
-        const cost=e.target.parentNode.childNodes[3].childNodes[1].innerText
-
-       const selectedContainer=document.getElementById("selected-place-container");
- 
-
-       const li=document.createElement("li");
-       li.style.display="flex";
-       li.style.flexDirection="row";
-       li.style.gap="10px";
-       const p=document.createElement("p");
-       const p2=document.createElement("p2");
-       p.innerText=placeName;
-       p2.innerText=cost;
-
-       li.appendChild(p);
-       li.appendChild(p2);
-       selectedContainer.appendChild(li);
-       
-       const total_cost=document.getElementById("Total_costs").innerText;
-       const convertedtotalcost=parseInt(total_cost);
-       document.getElementById("Total_costs").innerText=convertedtotalcost+parseInt(cost);
-
-
-
-        setInnerText("cart_count",count);
+         
+      setInnertext("cart_count",count);
         
-})
-}
+    })
+ }
+
+ function setInnertext(id,value){
+   document.getElementById(id).innerText=value;
+ }
 
 
 
-function setInnerText(id,value){
-    document.getElementById(id).innerText=value;
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const allbtn=document.getElementsByClassName("all-btn");
+// let count=0;
+// for(const btn of allbtn){
+//     btn.addEventListener("click",function(e){
+//        count=count+1;
+      
+//     const placeName=e.target.parentNode.childNodes[1].innerText;
+//     const cost = e.target.parentNode.childNodes[3].childNodes[1].innerText;
+
+//     const selectedContainer=document.getElementById("selected-place-container");
+//     const li=document.createElement("li");
+//     li.style.display="flex";
+//     li.style.flexDirection="row";
+//     li.style.gap="10px";
+//     const p=document.createElement("p");
+//     p.innerText=placeName;
+//     const p2=document.createElement("p2");
+//     p2.innerText=cost;
+//     li.appendChild(p);
+//     li.appendChild(p2);
+//     selectedContainer.appendChild(li);
+
+//     const total_cost=document.getElementById("Total_costs").innerText;
+//     const convertedtotalcost1=parseInt(total_cost);
+//     const sum1=convertedtotalcost1+parseInt(cost);
+    
+    
+
+//     const grand_cost=document.getElementById("grand_total").innerText;
+//     const convertedtotalcost2=parseInt(grand_cost);
+//     const sum2 = convertedtotalcost2+parseInt(cost);
+
+//     setInnertext("Total_costs", sum1);
+
+//     setInnertext("grand_total", sum2);
+
+//     setInnertext("cart_count",count);
+
+//     });
+
+// }
+
+// function setInnertext(id,value){
+//     document.getElementById(id).innerText=value;
+
+// }
+
+
 
 
 
