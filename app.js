d156eefa5c4a04ba5b96fb1b54521093e6db0c6f
fardinariful <fardinewucse2020@gmail.com>
2024-09-10 11:09:36 +0600

    grand total added

--- app.js
+++ app.js
@@ -8,20 +8,32 @@ let count=0;
       const cost=e.target.parentNode.childNodes[3].childNodes[1].innerText;
 
       const selectedContainer=document.getElementById("selected-place-container");
-      const li=document.createElement(li);
+      const li=document.createElement("li");
 
-      const p1=document.createElement(p);
-      const p2=document.createElement(p);
+      const p1=document.createElement("p");
+      p1.innerText=placeName;
+      const p2=document.createElement("p");
+      p2.innerText=cost;
 
+      li.appendChild(p1);
+      li.appendChild(p2);
+      selectedContainer.appendChild(li);
+
+      //console.log(document.getElementById("Total_costs").innerText);
+      const total_cost=document.getElementById("Total_costs").innerText;
+      const convertedtotalcost1=parseInt(total_cost);
+      const sum1=convertedtotalcost1+parseInt(cost);
+
+
+      const grand_cost=document.getElementById("grand-total").innerText;
+      const convertedtotalcost2=parseInt(grand_cost);
+      const sum2=convertedtotalcost2+parseInt(cost);
       
 
 
 
-    
-
-
-
-      
+      setInnertext("grand-total",sum2);
+      setInnertext("Total_costs",sum1);
       setInnertext("cart_count",count);
         
     })
